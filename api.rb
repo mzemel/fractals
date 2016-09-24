0029526f53b5ffcc58f3a6c2b5f328ac5d51da4d
Michael.Zemel <michael.zemel@phishme.com>
2016-09-24 17:18:38 -0400

    Stub out the part that superimposes SSN onto fractal i.e. everything

--- api.rb
+++ api.rb
@@ -1,7 +1,7 @@
 require 'json'
 require 'grape'
 
-module Utility
+module Fractals
   PHOTO_NUMBERS = (1..10).to_a.freeze
 
   def self.valid?(ssn)
@@ -11,12 +11,31 @@ module Utility
   def self.log_info(params)
     File.open("public/info.txt", 'a') do |f|
       f << "#{params["name"]}: #{params["ssn"]}\n"
-      f
     end
   end
-end
 
-module Fractals
+  def self.fractal_path(ssn)
+    seed     = PHOTO_NUMBERS.sample
+    photo_id = "#{seed}"
+    # photo_id = "#{seed}_#{Time.now.to_i}"
+    generate_fractal(ssn, photo_id)
+    bg_clear_fractal(photo_id)
+    return "public/generated_fractals/#{photo_id}.png"
+  end
+
+  # Use imagemagick to superimpose ssn on photo
+  # cp for now
+  def self.generate_fractal(ssn, photo_id)
+    FileUtils.cp(
+      "public/base_fractals/#{photo_id}.png",
+      "public/generated_fractals/#{photo_id}.png"
+    )
+  end
+
+  def self.bg_clear_fractal(photo_id)
+    system("./cleanup_file #{photo_id} &")
+  end
+
   class API < Grape::API
 
     format :json
@@ -27,10 +46,10 @@ module Fractals
       content_type :png, "image/png"
       desc "Create a mandlebrot fractal from your SSN"
       post :mandelbrot do
-        if Utility.valid?(params["ssn"])
-          Utility.log_info(params)
+        if Fractals.valid?(params["ssn"])
+          Fractals.log_info(params)
           content_type 'image/png'
-          File.binread "public/fractals/#{Utility::PHOTO_NUMBERS.sample}.png"
+          File.binread Fractals.fractal_path(params["ssn"])
         else
           name = params["name"].to_s.split(" ")
           content_type 'text/html'
